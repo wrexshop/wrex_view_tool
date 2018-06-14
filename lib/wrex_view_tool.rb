40344a81e910dc38c22f4524298ff33565b1edec
justin <ramos.justin21@gmail.com>
2018-06-14 15:56:19 -0700

    View tool initial commit

---
+++ lib/wrex_view_tool.rb
@@ -0,0 +1,6 @@
+require "wrex_view_tool/version"
+require "wrex_view_tool/renderer"
+
+module WrexViewTool
+  # Your code goes here...
+end
---
+++ lib/wrex_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WrexViewTool
+  VERSION = "0.1.0"
+end
